e9d7d4ecf0cac4222b72986426784067a507e2ee
viktor <vi.dave@yandex.ru>
2023-05-05 05:56:06 +0300

    [other] add golangci config [other] Style fix [upd] add body close for responses [upd] add method LeadDiary

--- client.go
+++ client.go
@@ -1,10 +1,12 @@
-package calltouch_sdk
+package calltouch
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -14,111 +16,125 @@ import (
 const DateFormat = "02/01/2006"
 
 type Client struct {
-	AccessToken string
+	accessToken string
 }
 
 func NewClient(accessToken string) *Client {
 	return &Client{
-		AccessToken: accessToken,
+		accessToken: accessToken,
 	}
 }
 
-//func (c *Client) OrdersDiary(siteID int, dateFrom time.Time, dateTo time.Time) (err error) {
-//	if dateFrom.After(dateTo) {
-//		return errors.New("dateFrom must be before dateTo")
-//	}
-//
-//	u, err := c.urlBuilder("orders-diary/orders", siteID, dateFrom, dateTo)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//
-//	req, err := http.Get(u.String())
-//	if err != nil {
-//		return err
-//	}
-//	defer req.Body.Close()
-//
-//	if req.StatusCode != 200 {
-//		return errors.New(fmt.Sprintf("status code: %v", req.StatusCode))
-//	}
-//
-//	responseBody, err := io.ReadAll(req.Body)
-//	if err != nil {
-//		return err
-//	}
-//
-//	var data string
-//	err = json.Unmarshal(responseBody, &data)
-//	if err != nil {
-//		return err
-//	}
-//
-//	return err
-//}
+type Period struct {
+	DateFrom time.Time
+	DateTo   time.Time
+}
 
-func (c *Client) urlBuilder(method string, siteID int, dateFrom time.Time, dateTo time.Time, page int) (u url.URL, err error) {
-	if dateFrom.After(dateTo) {
-		return u, errors.New("dateFrom must be before dateTo")
+type CallOptions struct {
+	uniqueOnly        bool // Флаг выгрузки уникальных звонков.
+	targetOnly        bool // Флаг выгрузки целевых звонков.
+	uniqTargetOnly    bool // Флаг выгрузки уникально-целевых звонков.
+	callbackOnly      bool // Флаг выгрузки обратных звонков.
+	withMapVisits     bool // Флаг истории посещений посетителя, совершившего звонок.
+	withOrders        bool // Флаг связки звонка со сделкой.
+	withCallTags      bool // Флаг отображения тегов звонков.
+	withComments      bool // Флаг отображения комментариев к звонкам, оставленных в плеере журнала звонков.
+	withYandexDirect  bool // Флаг выгрузки данных по рекламным кампаниям Яндекс.Директ.
+	withGoogleAdwords bool // Флаг выгрузки данных по рекламным кампаниям Google AdWords.
+	withText          bool // Флаг выгрузки разговора звонка в текстовой форме.
+	withDcm           bool // Флаг выгрузки данных по интеграции с DoubleClick Campaign Manager
+}
+
+func (c *Client) CallsDiary(ctx context.Context, siteID int, period Period, options *CallOptions) ([]Call, error) {
+	page := 0
+	calls := make([]Call, 0)
+
+	var isOk bool
+	for !isOk {
+		page++
+
+		u, bErr := c.callURLBuilder("calls-diary/calls", siteID, period, page, options)
+		if bErr != nil {
+			return nil, bErr
+		}
+
+		req, reqErr := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
+		if reqErr != nil {
+			return nil, reqErr
+		}
+
+		resp, respErr := http.DefaultClient.Do(req)
+		if respErr != nil {
+			return nil, respErr
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("status code: %v", resp.StatusCode)
+		}
+
+		responseBody, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		var data CallReport
+
+		jErr := json.Unmarshal(responseBody, &data)
+		if err != nil {
+			return nil, jErr
+		}
+
+		calls = append(calls, data.Records...)
+		isOk = data.PageTotal == data.Page
+
+		closeErr := resp.Body.Close()
+		if closeErr != nil {
+			log.Println(closeErr)
+		}
 	}
 
-	dateFromString := dateFrom.Format(DateFormat)
-	dateToString := dateTo.Format(DateFormat)
+	return calls, nil
+}
 
-	u = url.URL{
+func (c *Client) callURLBuilder(method string, siteID int, period Period, page int, options *CallOptions) (url.URL, error) {
+	if period.DateFrom.After(period.DateTo) {
+		return url.URL{}, errors.New("dateFrom must be before dateTo")
+	}
+
+	dateFromString := period.DateFrom.Format(DateFormat)
+	dateToString := period.DateTo.Format(DateFormat)
+
+	u := url.URL{
 		Scheme: "https",
 		Host:   "api.calltouch.ru",
 		Path:   fmt.Sprintf("calls-service/RestAPI/%v/%s", siteID, method),
 	}
 
 	params := url.Values{}
-	params.Add("clientApiId", c.AccessToken)
+	params.Add("clientApiId", c.accessToken)
 	params.Add("dateFrom", dateFromString)
 	params.Add("dateTo", dateToString)
 	params.Add("page", strconv.Itoa(page))
 	params.Add("limit", "1000")
+
+	if options != nil {
+		params.Add("uniqueOnly", strconv.FormatBool(options.uniqueOnly))
+		params.Add("targetOnly", strconv.FormatBool(options.targetOnly))
+		params.Add("uniqTargetOnly", strconv.FormatBool(options.uniqTargetOnly))
+		params.Add("callbackOnly", strconv.FormatBool(options.callbackOnly))
+		params.Add("withMapVisits", strconv.FormatBool(options.withMapVisits))
+		params.Add("withOrders", strconv.FormatBool(options.withOrders))
+		params.Add("withCallTags", strconv.FormatBool(options.withCallTags))
+		params.Add("withComments", strconv.FormatBool(options.withComments))
+		params.Add("withYandexDirect", strconv.FormatBool(options.withYandexDirect))
+		params.Add("withGoogleAdwords", strconv.FormatBool(options.withGoogleAdwords))
+		params.Add("withText", strconv.FormatBool(options.withText))
+		params.Add("withDcm", strconv.FormatBool(options.withDcm))
+	}
+
 	u.RawQuery = params.Encode()
 
 	return u, nil
-}
-
-func (c *Client) CallsDiary(siteID int, dateFrom time.Time, dateTo time.Time) (calls []Call, err error) {
-	isOk := false
-	page := 0
-	for !isOk {
-		page++
-		u, err := c.urlBuilder("calls-diary/calls", siteID, dateFrom, dateTo, page)
-		if err != nil {
-			return nil, err
-		}
-
-		req, err := http.Get(u.String())
-		if err != nil {
-			return nil, err
-		}
-		defer req.Body.Close()
-
-		if req.StatusCode != 200 {
-			return nil, errors.New(fmt.Sprintf("status code: %v", req.StatusCode))
-		}
-
-		responseBody, err := io.ReadAll(req.Body)
-		if err != nil {
-			return nil, err
-		}
-
-		var data CallReport
-		err = json.Unmarshal(responseBody, &data)
-		if err != nil {
-			return nil, err
-		}
-
-		calls = append(calls, data.Records...)
-		isOk = data.PageTotal == data.Page
-	}
-
-	return calls, nil
 }
 
 type CallReport struct {
@@ -127,4 +143,81 @@ type CallReport struct {
 	PageSize     int    `json:"pageSize"`
 	RecordsTotal int    `json:"recordsTotal"`
 	Records      []Call `json:"records"`
+}
+
+type LeadOptions struct {
+	withMapVisits     bool // Флаг истории посещений посетителя, совершившего звонок.
+	withRequestTags   bool // Флаг выгрузки тегов, которые были присвоены заявкам.
+	withYandexDirect  bool // Флаг выгрузки данных по рекламным кампаниям Яндекс.Директ.
+	withGoogleAdwords bool // Флаг выгрузки данных по рекламным кампаниям Google AdWords.
+	withDcm           bool // Флаг выгрузки данных по интеграции с DoubleClick Campaign Manager
+}
+
+func (c *Client) LeadsDiary(ctx context.Context, period Period, options *LeadOptions) ([]Lead, error) {
+	u, err := c.leadURLBuilder(period, options)
+	if err != nil {
+		return nil, err
+	}
+
+	req, reqErr := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
+	if reqErr != nil {
+		return nil, reqErr
+	}
+
+	resp, respErr := http.DefaultClient.Do(req)
+	if respErr != nil {
+		return nil, respErr
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("status code: %v", resp.StatusCode)
+	}
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var leads []Lead
+
+	err = json.Unmarshal(responseBody, &leads)
+	if err != nil {
+		return nil, err
+	}
+
+	return leads, nil
+}
+
+func (c *Client) leadURLBuilder(period Period, options *LeadOptions) (url.URL, error) {
+	if period.DateFrom.After(period.DateTo) {
+		return url.URL{}, errors.New("dateFrom must be before dateTo")
+	}
+
+	dateFromString := period.DateFrom.Format(DateFormat)
+	dateToString := period.DateTo.Format(DateFormat)
+
+	u := url.URL{
+		Scheme: "https",
+		Host:   "api.calltouch.ru",
+		Path:   "calls-service/RestAPI/requests/",
+	}
+
+	params := url.Values{}
+	params.Add("clientApiId", c.accessToken)
+	params.Add("dateFrom", dateFromString)
+	params.Add("dateTo", dateToString)
+
+	if options != nil {
+		params.Add("withMapVisits", strconv.FormatBool(options.withMapVisits))
+		params.Add("withRequestTags", strconv.FormatBool(options.withRequestTags))
+		params.Add("withYandexDirect", strconv.FormatBool(options.withYandexDirect))
+		params.Add("withGoogleAdwords", strconv.FormatBool(options.withGoogleAdwords))
+		params.Add("withDcm", strconv.FormatBool(options.withDcm))
+	}
+
+	u.RawQuery = params.Encode()
+
+	return u, nil
 }
--- entity.go
+++ entity.go
@@ -1,11 +1,11 @@
-package calltouch_sdk
+package calltouch
 
 type Order struct {
 	CompletedAmount int    `json:"completedAmount"`
 	CompletedDate   string `json:"completedDate"`
 	CreatedDate     string `json:"createdDate"`
 	OrderDate       string `json:"orderDate"`
-	OrderId         int    `json:"orderId"`
+	OrderID         int    `json:"orderId"`
 	OrderNumber     string `json:"orderNumber"`
 	PlannedAmount   int    `json:"plannedAmount"`
 }
@@ -17,43 +17,234 @@ type Tag struct {
 }
 
 type Call struct {
-	Date            string   `json:"date"`
-	CallUrl         string   `json:"callUrl"`
-	City            string   `json:"city"`
-	UniqueCall      bool     `json:"uniqueCall"`
-	RedirectNumber  string   `json:"redirectNumber"`
-	CallReferenceId string   `json:"callReferenceId"`
-	UtmContent      string   `json:"utmContent"`
-	YaClientId      string   `json:"yaClientId"`
-	Source          string   `json:"source"`
-	Medium          string   `json:"medium"`
-	CallPhase       string   `json:"callphase"`
-	Duration        int      `json:"duration"`
-	Ref             string   `json:"ref"`
-	AdditionalTags  []string `json:"additionalTags"`
-	Hostname        string   `json:"hostname"`
-	WaitingConnect  int      `json:"waitingConnect"`
-	CtCallerId      string   `json:"ctCallerId"`
-	CallbackCall    bool     `json:"callbackCall"`
-	Keyword         string   `json:"keyword"`
-	Successful      bool     `json:"successful"`
-	Order           Order    `json:"order"`
-	CallId          int      `json:"callId"`
-	CallTags        []Tag    `json:"callTags"`
-	UtmSource       string   `json:"utmSource"`
-	SipCallId       string   `json:"sipCallId"`
-	CallerNumber    string   `json:"callerNumber"`
-	Ip              string   `json:"ip"`
-	UtmTerm         string   `json:"utmTerm"`
-	UserAgent       string   `json:"userAgent"`
-	SessionId       int      `json:"sessionId"`
-	UtmCampaign     string   `json:"utmCampaign"`
-	Url             string   `json:"url"`
-	Attrs           string   `json:"attrs"`
-	PhoneNumber     string   `json:"phoneNumber"`
-	UniqTargetCall  bool     `json:"uniqTargetCall"`
-	TargetCall      bool     `json:"targetCall"`
-	Attribution     int      `json:"attribution"`
-	UtmMedium       string   `json:"utmMedium"`
-	Orders          []Order  `json:"orders"`
+	CallID          int            `json:"callId"`          // Уникальный идентификатор звонка в Calltouch.
+	Callphase       string         `json:"callphase"`       // Фаза звонка на момент API запроса:
+	Attribution     int            `json:"attribution"`     // Модель атрибуции звонков.
+	CallTags        *[]Tag         `json:"callTags"`        // Теги звонков
+	Date            string         `json:"date"`            // Дата звонка.
+	Duration        int            `json:"duration"`        // Длительность разговора.
+	CallerNumber    string         `json:"callerNumber"`    // Номер звонившего.
+	RedirectNumber  string         `json:"redirectNumber"`  // Номер переадресации.
+	PhoneNumber     string         `json:"phoneNumber"`     // Номер, на который звонили.
+	Manager         string         `json:"manager"`         // ФИО менеджера, который был присвоен этому звонку с помощью API-метода присвоения менеджеров к лидам.
+	Successful      bool           `json:"successful"`      // Статус звонка.
+	UniqueCall      bool           `json:"uniqueCall"`      // Уникальный звонок.
+	TargetCall      bool           `json:"targetCall"`      // Целевой звонок.
+	UniqTargetCall  bool           `json:"uniqTargetCall"`  // Уникально-целевой звонок.
+	CallbackCall    bool           `json:"callbackCall"`    // Обратный звонок.
+	City            string         `json:"city"`            // Город, в котором находится клиент, совершивший звонок.
+	Source          string         `json:"source"`          // Источник
+	Medium          string         `json:"medium"`          // Тип трафика.
+	Keyword         string         `json:"keyword"`         // Ключевое слово.
+	URL             string         `json:"url"`             // Адрес, по которому попали на сайт.
+	CallURL         string         `json:"callUrl"`         // Адрес страницы, находясь на которой, посетитель совершил звонок.
+	Ref             string         `json:"ref"`             // Адрес, с которого перешли на сайт.
+	Hostname        string         `json:"hostname"`        // Отслеживаемый домен или поддомен ресурса, на который был осуществлен переход (например: yoursite.ru).
+	UtmSource       string         `json:"utmSource"`       // Значение utm-метки utm_source.
+	UtmMedium       string         `json:"utmMedium"`       // Значение utm-метки utm_medium.
+	UtmCampaign     string         `json:"utmCampaign"`     // Значение utm-метки utm_campaign.
+	UtmContent      string         `json:"utmContent"`      // Значение utm-метки utm_content.
+	UtmTerm         string         `json:"utmTerm"`         // Значение utm-метки utm_term.
+	SessionID       int            `json:"sessionId"`       // Уникальный идентификатор сессии Calltouch.
+	CtCallerID      string         `json:"ctCallerId"`      // Уникальный идентификатор клиента в Calltouch.
+	ClientID        *string        `json:"clientId"`        //Уникальный идентификатор Universal Analytics.
+	YaClientID      *string        `json:"yaClientId"`      //Уникальный идентификатор Яндекс.Метрика.
+	SIPCallID       string         `json:"sipCallId"`       // Уникальный идентификатор сеанса связи с АТС Calltouch.
+	UserAgent       string         `json:"userAgent"`       // Информация об устройстве, с которого зашли на сайт.
+	IP              string         `json:"ip"`              // IP-адрес посетителя.
+	WaitingConnect  int            `json:"waitingConnect"`  // Время ожидания ответа.
+	CallReferenceID string         `json:"callReferenceId"` // Уникальный ID звонка с Вашей АТС, переданный в параметре callid API-запроса
+	MapVisits       *[]MapVisits   `json:"mapVisits"`       // История посещений.
+	Attrs           interface{}    `json:"attrs"`           // Сторонние параметры, переданные заранее в статистику Calltouch.
+	Comments        *[]Comment     `json:"comments"`        // Комментарии к звонкам, оставленные в плеере журнала звонков.
+	Phrases         *[]Phrase      `json:"phrases"`         // массив фраз из звонка (соблюдая последовательность)
+	AdditionalTags  []ValueField   `json:"additionalTags"`  // Дополнительные параметры отслеживания платного трафика.
+	Orders          []Order        `json:"orders"`          // Массив всех сделок, связанных со звонком.
+	YandexDirect    *YandexDirect  `json:"yandexDirect"`    // Данные для Яндекс.Директа.
+	GoogleAdWords   *GoogleAdWords `json:"googleAdWords"`   // Данные для GoogleAds.
+	CallbackInfo    CallBackInfo   `json:"callbackInfo"`    // Данные (ФИО, email, номер телефона) из форм обратного звонка, оставленных в соц. сетях
+	CtClientID      *int           `json:"ctClientId"`      // Идентификатор посетителя Calltouch. Он представляет из себя значение нашей куки _ct.
+	DCM             *DCM           `json:"dcm"`             // Данные по отправке звонка с DoubleClick Campaign Manager.
+	PhonesInText    *[]string      `json:"phonesInText"`    //Массив номеров телефонов, полученных из текста разговора (номера были произнесены в ходе разговора)
+	CtGlobalID      *int           `json:"ctGlobalId"`      // Глобальный идентификатор посетителя Calltouch, общий для сайтов, на которых установлен скрипт Calltouch.
+	SubPoolName     *string        `json:"subPoolName"`     // Название сабпула, с которым связан рекламный номер.
+	StatusDetails   string         `json:"statusDetails"`   // Детализация статуса звонка.
+}
+
+type Lead struct {
+	Date              int64          `json:"date"`
+	Comments          []Comment      `json:"comments"`
+	RequestType       string         `json:"requestType"`
+	DateStr           string         `json:"dateStr"`
+	Manager           string         `json:"manager"`
+	Session           Session        `json:"session"`
+	Subject           string         `json:"subject"`
+	UniqTargetRequest bool           `json:"uniqTargetRequest"`
+	UniqueRequest     bool           `json:"uniqueRequest"`
+	YandexDirect      *YandexDirect  `json:"yandexDirect"`
+	GoogleAdWords     *GoogleAdWords `json:"googleAdWords"`
+	RequestNumber     string         `json:"requestNumber"`
+	RequestID         int            `json:"requestId"`
+	Client            ClientInfo     `json:"client"`
+	SiteID            int            `json:"siteId"`
+	Orders            []LeadOrder    `json:"orders"`
+	TargetRequest     bool           `json:"targetRequest"`
+	Status            string         `json:"status"`
+	Order             interface{}    `json:"order"`
+	MapVisits         []MapVisits    `json:"mapVisits"`
+	RequestURL        *string        `json:"requestUrl"`
+	CtClientID        *int64         `json:"ctClientId"`
+	DCM               *[]DCM         `json:"dcm"`
+	CtGlobalID        *int           `json:"ctGlobalId"`
+	WidgetInfo        interface{}    `json:"widgetInfo"`
+	RequestTags       *[]RequestTag  `json:"RequestTags"`
+}
+
+type Comment struct {
+	CommentID int    `json:"commentId"`
+	RequestID int    `json:"requestId"`
+	Comment   string `json:"comment"`
+	PartyID   int    `json:"partyId"`
+	PartyName string `json:"partyName"`
+}
+
+type Session struct {
+	SessionID      int           `json:"sessionId"`
+	Keywords       string        `json:"keywords"`
+	City           string        `json:"city"`
+	IP             string        `json:"ip"`
+	Source         string        `json:"source"`
+	Medium         string        `json:"medium"`
+	Ref            string        `json:"ref"`
+	URL            string        `json:"url"`
+	UtmSource      string        `json:"utmSource"`
+	UtmMedium      string        `json:"utmMedium"`
+	UtmTerm        string        `json:"utmTerm"`
+	UtmContent     string        `json:"utmContent"`
+	UtmCampaign    string        `json:"utmCampaign"`
+	GuaClientID    string        `json:"guaClientId"`
+	SessionDate    string        `json:"sessionDate"`
+	Attrs          interface{}   `json:"attrs"`
+	Attribution    int           `json:"attribution"`
+	YaClientID     string        `json:"yaClientId"`
+	AdditionalTags []interface{} `json:"additionalTags"`
+	CtGlobalID     *int          `json:"ctGlobalId"`
+	Browser        string        `json:"browser"`
+}
+
+type ClientInfo struct {
+	ClientID int    `json:"clientId"`
+	Fio      string `json:"fio"`
+	Phones   []struct {
+		PhoneNumber string `json:"phoneNumber"`
+		PhoneType   string `json:"phoneType"`
+	} `json:"phones"`
+	Contacts []struct {
+		ContactType  string `json:"contactType"`
+		ContactValue string `json:"contactValue"`
+	} `json:"contacts"`
+}
+
+type LeadOrder struct {
+	OrderID       int         `json:"orderId"`
+	CallID        *int        `json:"callId"`
+	DateCreated   int64       `json:"dateCreated"`
+	Status        string      `json:"status"`
+	RealSum       string      `json:"realSum"`
+	Offered       interface{} `json:"offered"`
+	Sent          string      `json:"sent"`
+	Sum           string      `json:"sum"`
+	IsMarked      interface{} `json:"isMarked"`
+	CommentsCount int         `json:"commentsCount"`
+	CurrentAmount int         `json:"currentAmount"`
+	OrderNumber   string      `json:"orderNumber"`
+	OrderSum      string      `json:"orderSum"`
+	OrderStatus   string      `json:"orderStatus"`
+	OrderComments string      `json:"orderComments"`
+	Session       struct {
+		Keywords       string        `json:"keywords"`
+		City           string        `json:"city"`
+		IP             string        `json:"ip"`
+		Browser        string        `json:"browser"`
+		Source         string        `json:"source"`
+		Medium         string        `json:"medium"`
+		Ref            string        `json:"ref"`
+		URL            string        `json:"url"`
+		UtmSource      string        `json:"utmSource"`
+		UtmMedium      string        `json:"utmMedium"`
+		UtmTerm        string        `json:"utmTerm"`
+		UtmContent     string        `json:"utmContent"`
+		UtmCampaign    string        `json:"utmCampaign"`
+		GuaClientID    interface{}   `json:"guaClientId"`
+		YaClientID     interface{}   `json:"yaClientId"`
+		SessionID      int           `json:"sessionId"`
+		AdditionalTags []interface{} `json:"additionalTags"`
+		Attribution    int           `json:"attribution"`
+		Attrs          interface{}   `json:"attrs"`
+	} `json:"session"`
+}
+
+type YandexDirect struct {
+	CampaignID int   `json:"campaignId"`
+	AdGroupID  int64 `json:"adGroupId"`
+	AdID       int64 `json:"adId"`
+	CriteriaID int   `json:"criteriaId"`
+}
+
+type GoogleAdWords struct {
+	CampaignID int   `json:"campaignId"`
+	AdGroupID  int   `json:"adGroupId"`
+	CreativeID int   `json:"creativeId"`
+	CriteriaID int64 `json:"criteriaId"`
+}
+
+type DCM struct {
+	ProfileIDDCM              int    `json:"profileIdDCM"`
+	FloodlightConfigurationID string `json:"floodlightConfigurationId"`
+	FloodlightActivityID      string `json:"floodlightActivityId"`
+	RequestStatus             string `json:"requestStatus"`
+	RequestErrors             string `json:"requestErrors"`
+}
+
+type MapVisits struct {
+	UtmSource      string        `json:"utmSource"`
+	SessionDate    string        `json:"sessionDate"`
+	City           string        `json:"city"`
+	IP             string        `json:"ip"`
+	UtmTerm        string        `json:"utmTerm"`
+	UtmContent     string        `json:"utmContent"`
+	UserAgent      string        `json:"userAgent"`
+	SessionID      int           `json:"sessionId"`
+	Source         string        `json:"source"`
+	Medium         string        `json:"medium"`
+	UtmCampaign    string        `json:"utmCampaign"`
+	URL            string        `json:"url"`
+	Ref            string        `json:"ref"`
+	AdditionalTags []interface{} `json:"additionalTags"`
+	UtmMedium      string        `json:"utmMedium"`
+	GuaClientID    string        `json:"guaClientId"`
+	Keyword        string        `json:"keyword"`
+	CtClientID     int64         `json:"ctClientId"`
+	CtGlobalID     *int          `json:"ctGlobalId"`
+}
+
+type RequestTag []struct {
+	Category string   `json:"category"`
+	Type     string   `json:"type"`
+	Names    []string `json:"names"`
+}
+
+type Phrase struct {
+	Channel int    `json:"channel"`
+	Time    string `json:"time"`
+	Message string `json:"message"`
+}
+
+type CallBackInfo struct {
+	Fields []ValueField `json:"fields"`
+}
+
+type ValueField struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
 }
